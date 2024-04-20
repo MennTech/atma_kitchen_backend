b2ae9e621a4a4146ec7bba8258fa2725386604f3
dankski1337 <simonagis23@gmail.com>
2024-04-20 22:39:42 +0700

    edit request validation

--- app/Http/Controllers/Api/AuthCustomerController.php
+++ app/Http/Controllers/Api/AuthCustomerController.php
@@ -5,25 +5,42 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthCustomerController extends Controller
 {
     public function login (Request $request){
-        $data = $request->validate([
+        $requestData = $request->all();
+        $validator = Validator::make($requestData, [
             'email_customer' => 'required|email',
             'password' => 'required'
+        ],[
+            'email_customer.required' => 'Email wajib diisi',
+            'email_customer.email' => 'Email tidak valid',
+            'password.required' => 'Password wajib diisi'
         ]);
 
-        if(!Auth::guard('api_customer')->attempt($data)){
+        if($validator->fails()){
             return response()->json([
-                'message' => 'Email atau password salah'
+                'success' => false,
+                'message' => 'Login Gagal',
+                'error' => $validator->errors()
+            ], 400);
+        }
+
+        if(!Auth::guard('api_customer')->attempt($requestData)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Gagal',
+                'error' => 'Email atau Password Salah'
             ], 401);
         }
         /** @var \App\Models\Customer $customer **/
         $customer = Auth::guard('api_customer')->user();
         $token = $customer->createToken('customer-token')->plainTextToken;
         return response()->json([
-            'message' => 'Login Success',
+            'success' => true,
+            'message' => 'Login sukses',
             'customer' => $customer,
             'token_type' => 'Bearer',
             'token' => $token
@@ -36,17 +53,20 @@ class AuthCustomerController extends Controller
         $token = $customer->currentAccessToken();
         if(!$customer){
             return response()->json([
+                'success' => false,
                 'message' => 'Anda belum login'
             ], 401);
         }
         if($token->name !== 'customer-token'){
             return response()->json([
+                'success' => false,
                 'message' => 'Token tidak valid'
             ], 401);
         }
         $customer->tokens()->delete();
         return response()->json([
-            'message' => 'Logout Success'
+            'success' => true,
+            'message' => 'Logout sukses'
         ]);
     }
 }
